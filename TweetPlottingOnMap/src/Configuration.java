660f36155f05cc1ac2f198d58d01c3d8193d96dd
vinayakpalav <vvp221@nyu.edu>
2014-11-29 17:16:29 -0500

    Configuration file add

---
+++ TweetPlottingOnMap/src/Configuration.java
@@ -0,0 +1,15 @@
+
+public class Configuration {
+
+	public final static String twitterConsumerKey = "";
+	public final static String twitterConsumerSecret = "";
+	public final static String twitterAccessKey = "";
+	public final static String twitterTokenPrivate = "";
+	public final static String alchemyURL = "";
+	public final static String alchemyAPIKey = "";
+	public final static String queueName = "";
+	public final static String queueRegion = "";
+	public final static String dbURL = "";
+	public final static String dbUser = "";
+	public final static String dbPassword = "";
+}
